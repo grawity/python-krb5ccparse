42284928ca1c4b23d0a28b8643b7668efdb3ba0b
Mantas Mikulėnas <grawity@gmail.com>
2024-04-23 18:14:58 +0300

    finish ccache filtering/writing

--- lib/ccache.py
+++ lib/ccache.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass
-from pprint import pprint
 from .krb_base import *
 
 # Format documentation:
 #   https://web.mit.edu/kerberos/krb5-latest/doc/formats/ccache_file_format.html
+
+@dataclass
+class HeaderField:
+    tag: int
+    value: bytes
 
 @dataclass
 class CachedCredential:
@@ -66,41 +70,40 @@ class CacheReader(KrbBinaryReader):
         adata = self.read_data()
         return Authdata(adtype, adata)
 
-    def read_time(self):
-        return self.read_u32()
-
     def read_credential(self):
-        entry = {}
-        entry["client"] = self.read_principal()
-        entry["server"] = self.read_principal()
-        entry["keyblock"] = self.read_keyblock()
-        entry["authtime"] = self.read_time()
-        entry["starttime"] = self.read_time()
-        entry["endtime"] = self.read_time()
-        entry["renewtime"] = self.read_time()
-        entry["is_skey"] = self.read_u8()
-        entry["ticket_flags"] = self.read_u32()
-        entry["addresses"] = []
+        client = self.read_principal()
+        server = self.read_principal()
+        keyblock = self.read_keyblock()
+        authtime = self.read_time()
+        starttime = self.read_time()
+        endtime = self.read_time()
+        renewtime = self.read_time()
+        is_skey = bool(self.read_u8())
+        ticket_flags = self.read_u32()
         n_addresses = self.read_u32()
+        addresses = []
         for x in range(n_addresses):
-            entry["addresses"].append(self.read_address())
-        entry["authdata"] = []
+            addresses.append(self.read_address())
         n_authdata = self.read_u32()
+        authdata = []
         for x in range(n_authdata):
-            entry["authdata"].append(self.read_authdata())
-        entry["ticket"] = self.read_data()
-        entry["second_ticket"] = self.read_data()
-        return CachedCredential(**entry)
+            authdata.append(self.read_authdata())
+        ticket = self.read_data()
+        second_ticket = self.read_data()
+        return CachedCredential(client, server, keyblock, authtime,
+                                starttime, endtime, renewtime, is_skey,
+                                ticket_flags, addresses, authdata,
+                                ticket, second_ticket)
 
     def read_header(self):
         hdr_length = self.read_u16()
         end_pos = self.tell() + hdr_length
         fields = []
         while self.tell() < end_pos:
-            tlv_tag = self.read_u16()
-            tlv_length = self.read_u16()
-            tlv_value = self.read(tlv_length)
-            fields.append((tlv_tag, tlv_value))
+            tag = self.read_u16()
+            length = self.read_u16()
+            value = self.read(length)
+            fields.append(HeaderField(tag, value))
         return fields
 
     def read_cache(self):
@@ -127,21 +130,112 @@ class CacheReader(KrbBinaryReader):
                      default_princ,
                      credentials)
 
+class CacheWriter(KrbBinaryWriter):
+    def write_data(self, data):
+        self.write_u32(len(data))
+        self.write(data)
+
+    def write_principal(self, princ):
+        if self._version >= 2:
+            self.write_u32(princ.nametype)
+        n_components = len(princ.components)
+        if self._version == 1:
+            n_components += 1
+        self.write_u32(n_components)
+        self.write_data(princ.realm)
+        for x in princ.components:
+            self.write_data(x)
+
+    def write_keyblock(self, keyblock):
+        if self._version == 3:
+            self.write_u16(keyblock.enctype)
+        self.write_u16(keyblock.enctype)
+        self.write_data(keyblock.data)
+
+    def write_address(self, address):
+        self.write_u16(address.addrtype)
+        self.write_data(address.data)
+
+    def write_authdata(self, authdata):
+        self.write_u16(authdata.adtype)
+        self.write_data(authdata.data)
+
+    def write_credential(self, cred):
+        self.write_principal(cred.client)
+        self.write_principal(cred.server)
+        self.write_keyblock(cred.keyblock)
+        self.write_time(cred.authtime)
+        self.write_time(cred.starttime)
+        self.write_time(cred.endtime)
+        self.write_time(cred.renewtime)
+        self.write_u8(cred.is_skey)
+        self.write_u32(cred.ticket_flags)
+        self.write_u32(len(cred.addresses))
+        for x in cred.addresses:
+            self.write_address(x)
+        self.write_u32(len(cred.authdata))
+        for x in cred.authdata:
+            self.write_authdata(x)
+        self.write_data(cred.ticket)
+        self.write_data(cred.second_ticket)
+
+    def write_header(self, header):
+        hdr_length = sum([2 + 2 + len(x.value) for x in header])
+        self.write_u16(hdr_length)
+        for x in header:
+            self.write_u16(x.tag)
+            self.write_u16(len(x.value))
+            self.write(x.value)
+
+    def write_cache(self, cache: Cache):
+        self.write_u8(5)
+        self.write_u8(cache.version)
+        self._version = cache.version
+        self._uses_native_endian = (self._version <= 2)
+        if self._version == 4:
+            self.write_header(cache.header)
+        else:
+            assert not cache.header
+        self.write_principal(cache.default_princ)
+        for cred in cache.credentials:
+            self.write_credential(cred)
+
 if __name__ == "__main__":
     import argparse
     import os
+    from pprint import pprint
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="dump the cache contents to stdout")
     parser.add_argument("-p", "--principal", action="append", default=[],
                         help="principal to erase tickets for")
     args = parser.parse_args()
+
+    exclude = {*args.principal}
 
     try:
         cctype, ccname = os.environ["KRB5CCNAME"].split(":", 1)
     except KeyError:
         cctype, ccname = "FILE", "/tmp/krb5cc_%d" % os.getuid()
 
+    cache = None
     with open(ccname, "rb") as fh:
-        cr = CacheReader(fh)
-        c = cr.read_cache()
-        pprint(c)
+        cache = CacheReader(fh).read_cache()
+
+    creds = []
+    for cred in cache.credentials:
+        name, realm = cred.server.unparse().rsplit("@", 1)
+        if {f"{name}", f"{name}@{realm}"} & exclude:
+            print(f"Skipping ticket for {name}@{realm}")
+            continue
+        creds.append(cred)
+
+    if args.verbose:
+        pprint(cache)
+
+    cache.credentials = creds
+
+    with open("/tmp/krb5cc", "wb") as fh:
+        os.fchmod(fh.fileno(), 0o600)
+        CacheWriter(fh).write_cache(cache)
--- lib/krb_base.py
+++ lib/krb_base.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from .io_base import BinaryReader
+from .io_base import BinaryReader, BinaryWriter
 
 # The default name type
 KRB5_NT_PRINCIPAL = 1
@@ -53,9 +53,36 @@ class KrbBinaryReader(BinaryReader):
         else:
             return self._read_fmt(4, ">l")
 
+    def read_time(self):
+        return self.read_u32()
+
     def tell_eof(self):
         start_pos = self.tell()
         self.seek(0, 2)
         end_pos = self.tell()
         self.seek(start_pos)
         return end_pos
+
+class KrbBinaryWriter(BinaryWriter):
+    _uses_native_endian = True
+
+    def write_u16(self, value):
+        if self._uses_native_endian:
+            return self._write_fmt("H", value)
+        else:
+            return self.write_u16_be(value)
+
+    def write_u32(self, value):
+        if self._uses_native_endian:
+            return self._write_fmt("L", value)
+        else:
+            return self.write_u32_be(value)
+
+    def write_s32(self, value):
+        if self._uses_native_endian:
+            return self._write_fmt("l", value)
+        else:
+            return self._write_fmt(">l", value)
+
+    def write_time(self, value):
+        self.write_u32(value)
